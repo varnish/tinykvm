a5e6049a81461a7fd6194d0deac2476eaa85d74c
Alf-André Walla <fwsgonzo@hotmail.com>
2022-03-09 17:40:52 +0100

    Add cmdline arguments test and execution timeout test

--- tests/unit/basic.cpp
+++ tests/unit/basic.cpp
@@ -2,8 +2,11 @@
 
 #include <tinykvm/machine.hpp>
 extern std::vector<uint8_t> build_and_load(const std::string& code);
-static const uint64_t MAX_MEMORY = 8ul << 20; /* 8MB */
 extern void setup_kvm_system_calls();
+static const uint64_t MAX_MEMORY = 8ul << 20; /* 8MB */
+static const std::vector<std::string> env {
+	"LC_TYPE=C", "LC_ALL=C", "USER=root"
+};
 
 TEST_CASE("Initialize KVM", "[Initialize]")
 {
@@ -29,6 +32,41 @@ int main() {
 	REQUIRE(machine.start_address() > 0x400000);
 }
 
+TEST_CASE("Runtime setup and execution", "[Output]")
+{
+	const auto binary = build_and_load(R"M(
+#include <string.h>
+int main(int argc, char** argv) {
+	if (strcmp(argv[0], "are we passing this correctly?") == 0)
+		return 666;
+	else
+		return -1;
+})M");
+
+	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
+	// We need to create a Linux environment for runtimes to work well
+	machine.setup_linux({"are we passing this correctly?"}, env);
+	machine.run(2.0f);
+
+	REQUIRE(machine.return_value() == 666);
+}
+
+TEST_CASE("Execution timeout", "[Output]")
+{
+	const auto binary = build_and_load(R"M(
+#include <string.h>
+int main() {
+	while (1);
+})M");
+
+	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
+	// We need to create a Linux environment for runtimes to work well
+	machine.setup_linux({"are we passing this correctly?"}, env);
+	REQUIRE_THROWS([&] {
+		machine.run(1.0f);
+	}());
+}
+
 TEST_CASE("Catch output from write system call", "[Output]")
 {
 	bool output_is_hello_world = false;
@@ -40,9 +78,7 @@ int main() {
 
 	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
 	// We need to create a Linux environment for runtimes to work well
-	machine.setup_linux(
-		{"basic"},
-		{"LC_TYPE=C", "LC_ALL=C", "USER=root"});
+	machine.setup_linux({"basic"}, env);
 
 	machine.set_printer([&] (const char* data, size_t size) {
 		std::string text{data, data + size};
