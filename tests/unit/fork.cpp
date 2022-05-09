3b070668caf301ee5dcd207380f04590e6b2a4fb
Alf-André Walla <fwsgonzo@hotmail.com>
2022-05-09 13:56:42 +0200

    Verify running out of CoW memory

--- tests/unit/fork.cpp
+++ tests/unit/fork.cpp
@@ -1,9 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 
 #include <tinykvm/machine.hpp>
 extern std::vector<uint8_t> build_and_load(const std::string& code);
 extern void setup_kvm_system_calls();
 static const uint64_t MAX_MEMORY = 8ul << 20; /* 8MB */
+static const uint64_t MAX_COWMEM = 1ul << 20; /* 1MB */
 static const std::vector<std::string> env {
 	"LC_TYPE=C", "LC_ALL=C", "USER=root"
 };
@@ -29,7 +31,7 @@ extern void prints_hello_world() {
 
 	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
 	// We need to create a Linux environment for runtimes to work well
-	machine.setup_linux({"basic"}, env);
+	machine.setup_linux({"fork"}, env);
 
 	// Run for at most 4 seconds before giving up
 	machine.run(4.0f);
@@ -44,7 +46,7 @@ extern void prints_hello_world() {
 
 	// Create fork
 	auto fork = tinykvm::Machine { machine, {
-		.max_mem = MAX_MEMORY, .max_cow_mem = MAX_MEMORY
+		.max_mem = MAX_MEMORY, .max_cow_mem = MAX_COWMEM
 	} };
 	fork.set_printer([&] (const char* data, size_t size) {
 		std::string text{data, data + size};
@@ -59,4 +61,34 @@ extern void prints_hello_world() {
 
 	// Now the output should be written out
 	REQUIRE(output_is_hello_world);
+}
+
+TEST_CASE("Fork and run out of memory", "[Fork]")
+{
+	const auto binary = build_and_load(R"M(
+extern long write(int, const void*, unsigned long);
+int main() {
+}
+extern void callable_function() {
+})M");
+
+	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
+	// We need to create a Linux environment for runtimes to work well
+	machine.setup_linux({"fork"}, env);
+
+	// Run for at most 4 seconds before giving up
+	machine.run(4.0f);
+
+	// Make machine forkable
+	machine.prepare_copy_on_write();
+	REQUIRE(machine.is_forkable());
+	REQUIRE(!machine.is_forked());
+
+	REQUIRE_THROWS([&] () {
+		// Create fork that runs out of memory
+		tinykvm::MachineOptions options;
+		options.max_mem = MAX_MEMORY;
+		options.max_cow_mem = 0UL;
+		tinykvm::Machine fork { machine, options};
+	}());
 }
