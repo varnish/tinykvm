1706e05178e45050d6878a24e816e47765050374
Alf-André Walla <fwsgonzo@hotmail.com>
2022-10-03 08:40:27 +0200

    Add test for machine resets

---
+++ tests/unit/reset.cpp
@@ -0,0 +1,80 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+#include <tinykvm/machine.hpp>
+extern std::vector<uint8_t> build_and_load(const std::string& code);
+extern void setup_kvm_system_calls();
+static const uint64_t MAX_MEMORY = 32ul << 20; /* 32MB */
+static const uint64_t MAX_COWMEM =  8ul << 20; /* 8MB */
+static const std::vector<std::string> env {
+	"LC_TYPE=C", "LC_ALL=C", "USER=root"
+};
+
+TEST_CASE("Initialize KVM", "[Initialize]")
+{
+	// Create KVM file descriptors etc.
+	tinykvm::Machine::init();
+	// Install Linux and POSIX system call handlers
+	setup_kvm_system_calls();
+}
+
+TEST_CASE("Execute function in reset VM", "[Reset]")
+{
+	const auto binary = build_and_load(R"M(
+static int a = 0;
+static int b = 1;
+int main() {
+}
+extern long get_a() {
+	int ta = a;
+	a = 333;
+	return ta;
+}
+extern long get_b() {
+	int tb = b;
+	b = 666;
+	return tb;
+}
+extern long get_mmap(int *z) {
+	int total = z[100] + z[200] + z[300] + z[400];
+	z[100] = 22;
+	z[200] = 44;
+	z[300] = 66;
+	z[400] = 88;
+	return total;
+})M");
+
+	tinykvm::Machine machine { binary, { .max_mem = MAX_MEMORY } };
+	// We need to create a Linux environment for runtimes to work well
+	machine.setup_linux({"reset"}, env);
+
+	// Run for at most 4 seconds before giving up
+	machine.run(4.0f);
+	// Make machine forkable (no working memory)
+	machine.prepare_copy_on_write(0);
+
+	auto maddr = machine.mmap_allocate(0x1000);
+
+	// Create fork
+	auto fork = tinykvm::Machine { machine, {
+		.max_mem = MAX_MEMORY, .max_cow_mem = MAX_COWMEM
+	} };
+
+	for (size_t i = 0; i < 15; i++)
+	{
+		auto& m = fork;
+		m.timed_vmcall(m.address_of("get_a"), 2.0f);
+		REQUIRE(m.return_value() == 0);
+
+		m.timed_vmcall(m.address_of("get_b"), 2.0f);
+		REQUIRE(m.return_value() == 1);
+
+		m.timed_vmcall(m.address_of("get_mmap"), 2.0f, (uint64_t)maddr);
+		REQUIRE(m.return_value() == 0);
+
+		m.reset_to(machine, {
+			.max_mem = MAX_MEMORY,
+			.max_cow_mem = MAX_COWMEM
+		});
+	}
+}
