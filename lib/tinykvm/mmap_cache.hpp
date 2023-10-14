7cec7ea49a1911027a2fa1f5ded8d5f7ecb05823
Alf-André Walla <fwsgonzo@hotmail.com>
2023-10-14 18:50:37 +0200

    Improve MMAP cache to extend and shrink ranges, preserving memory

--- lib/tinykvm/mmap_cache.hpp
+++ lib/tinykvm/mmap_cache.hpp
@@ -15,45 +15,65 @@ namespace tinykvm
 				return ((this->addr >= mem) && (this->addr + this->size <= mem + memsize))
 					|| ((mem >= this->addr) && (mem + memsize <= this->addr + this->size));
 			}
+			constexpr bool equals(uint64_t mem, uint64_t memsize) const noexcept {
+				return (this->addr == mem) && (this->addr + this->size == mem + memsize);
+			}
 		};
 
 		Range find(uint64_t size)
 		{
-			for (auto& r : m_lines)
+			auto it = m_lines.begin();
+			while (it != m_lines.end())
 			{
+				auto& r = *it;
 				if (!r.empty())
 				{
 					if (r.size >= size) {
-						const auto ret = r;
-						r = Range{};
-						return ret;
+						const Range result { r.addr, size };
+						if (r.size > size) {
+							r.addr += size;
+							r.size -= size;
+						} else {
+							m_lines.erase(it);
+						}
+						return result;
 					}
 				}
+				++it;
 			}
 			return Range{};
 		}
 
 		void invalidate(uint64_t addr, uint64_t size)
 		{
-			for (auto& r : m_lines)
+			auto it = m_lines.begin();
+			while (it != m_lines.end())
 			{
+				const auto r = *it;
 				if (r.within(addr, size))
 				{
-					r = Range{};
+					bool equals = r.equals(addr, size);
+					it = m_lines.erase(it);
+					if (equals) return;
 				}
+				else ++it;
 			}
 		}
 
 		void insert(uint64_t addr, uint64_t size)
 		{
-			m_lines[m_rotate].addr = addr;
-			m_lines[m_rotate].size = size;
+			/* Extend the back range? */
+			if (!m_lines.empty()) {
+				if (m_lines.back().addr + m_lines.back().size == addr) {
+					m_lines.back().size += size;
+					return;
+				}
+			}
 
-			m_rotate = (m_rotate + 1) % m_lines.size();
+			m_lines.push_back({addr, size});
 		}
 
 	private:
-		std::array<Range, 8> m_lines {};
-		unsigned m_rotate = 0;
+		std::vector<Range> m_lines {};
 	};
 } // tinykvm
