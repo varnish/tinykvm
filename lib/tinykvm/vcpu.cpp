18c7a5f5b785f99856f36bf79481fae237f9f1d8
Alf-André Walla <fwsgonzo@hotmail.com>
2025-03-30 13:55:28 +0200

    We can zero the IST stack, avoiding a memory copy

--- lib/tinykvm/vcpu.cpp
+++ lib/tinykvm/vcpu.cpp
@@ -434,7 +434,7 @@ void Machine::setup_cow_mode(const Machine* other)
 	// stackless interrupts, to be honest. Something to think about?
 	// XXX: In theory we can avoid initializing one of these pages
 	// until the guest asks for a certain level of concurrency.
-	memory.get_writable_page(memory.physbase + IST_ADDR, PDE64_RW | PDE64_NX, false);
+	memory.get_writable_page(memory.physbase + IST_ADDR, PDE64_RW | PDE64_NX, true);
 	//memory.get_writable_page(memory.physbase + IST2_ADDR, PDE64_RW | PDE64_NX, true);
 
 	struct kvm_sregs sregs = other->get_special_registers();
