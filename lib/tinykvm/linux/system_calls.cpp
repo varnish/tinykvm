2837160db9ab60c1992f836b42eda8af4d73e3a2
Alf-André Walla <fwsgonzo@hotmail.com>
2025-05-01 21:51:44 +0200

    getpid() should return 1 to match TID=1

--- lib/tinykvm/linux/system_calls.cpp
+++ lib/tinykvm/linux/system_calls.cpp
@@ -794,7 +794,7 @@ void Machine::setup_linux_system_calls()
 	Machine::install_syscall_handler(
 		SYS_getpid, [](vCPU& cpu) { // GETPID
 			auto& regs = cpu.registers();
-			regs.rax = 0;
+			regs.rax = 1; // TID=1
 			cpu.set_registers(regs);
 			SYSPRINT("getpid() = %lld\n", regs.rax);
 		});
